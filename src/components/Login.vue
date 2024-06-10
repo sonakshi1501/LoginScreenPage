1596ba736fc4dcd90bffffe078e19c8d110b0a17
Sonakshi <sonakshishukla1501@gmail.com>
2024-06-10 15:45:21 +0530

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-app>
+      <v-container class="fill-height" fluid>
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card>
+              <v-card-title class="headline">Login</v-card-title>
+              <v-card-text>
+                <v-form ref="form">
+                  <v-text-field
+                    v-model="email"
+                    label="Email"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    label="Password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="loginPage">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+        loginPage() {
+        // Add your login logic here
+        console.log('Email:', this.email);
+        console.log('Password:', this.password);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .fill-height {
+    min-height: 100vh;
+  }
+  </style>
+  
